29de1c5af450089f74b6f8d189508e6331c317ac
unknown <48255311+Demonster01@users.noreply.github.com>
2019-05-16 11:43:55 +0300

    add startup demo

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,38 @@
 int pinCS = PA3; // Attach display CS to this pin (PA3), DIN to SPI1_MOSI (PA7) and CLK to SPI1_SCK (PA5)
 int numberOfHorizontalDisplays = 4;
 int numberOfVerticalDisplays = 1;
-intensity = 7; // Use a value between 0 and 15 for brightness
+int intensity = 7; // Use a value between 0 and 15 for brightness
 
 Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
 
 // analog
-const int audio_in = PB1;
+int audio_in = PB1;
+
+// demo string
+String tape = "@2019 ychurov@i.ua";
+int wait = 20; // In milliseconds
+int spacer = 1;
+int width = 5 + spacer; // The font width is 5 pixels
+int width1 = 0 - width; // need to draw escaping char
+
+// copyright & demo
+void startup_demo() {
+  for ( int i = 0 ; i < width * tape.length() + matrix.width() - 1 - spacer; i++ ) {
+    matrix.fillScreen(LOW);
+    int letter = i / width;
+    int x = (matrix.width() - 1) - i % width;
+    int y = (matrix.height() - 8) / 2; // center the text vertically
+    while ( x + width - spacer >= 0 && letter >= 0 ) {
+      if ( x > width1 && letter < tape.length() ) {
+        matrix.drawChar(x, y, tape[letter], HIGH, LOW, 1);
+      }
+      letter--;
+      x -= width;
+    }
+    matrix.write(); // Send bitmap to display
+    delay(wait);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,9 +55,8 @@ void setup() {
     matrix.setRotation(i, 1);    // all displays position is 90 degrees clockwise
   }
   matrix.setIntensity(intensity); // Use a value between 0 and 15 for brightness
-  // clear screen
-  matrix.fillScreen(LOW);
-  matrix.write();
+  // demo
+  startup_demo();
 }
 
 void loop() {
